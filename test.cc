d8740b5afd3b55b1d8294f93392fa17c01d3b5f5
fang_wsl_ubuntu22 <equations_list@163.com>
2024-08-15 11:21:33 +0800

    链接jsoncpp库

--- test.cc
+++ test.cc
@@ -1,11 +1,26 @@
 #include <iostream>
 #include <boost/version.hpp>
+#include <jsoncpp/json/json.h>
+#include <jsoncpp/json/value.h>
+#include <jsoncpp/json/writer.h>
 
 
 
-int main()
+int main(int argc, char** argv)
 {
     std::cout << "Boost库版本： " << BOOST_VERSION << std::endl;
+
+    Json::Value root;
+
+    Json::FastWriter fast;
+
+    root["ModuleType"] = Json::Value("你好~");
+    root["ModuleCode"] = Json::Value("22");
+    root["ModuleDesc"] = Json::Value("33");
+    root["DateTime"] = Json::Value("44");
+    root["LogType"] = Json::Value("55");
+
+    std::cout << fast.write(root) << std::endl;
 
     return 0;
 }
